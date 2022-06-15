b30154f7fe7b782cc27885a8cc68098b4a8f4a00
Kate Danilova <ecdanilova@gmail.com>
2022-06-15 10:54:52 -0400

    add snow effect

--- script.js
+++ script.js
@@ -43,3 +43,146 @@ const button = document.querySelector("#myButton");
 button.addEventListener("click", function() {
 document.querySelector("#myAudio").play();
 })
+
+// Snow effect
+
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 300,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 3,
+          "color": "#fff"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "http://www.dynamicdigital.us/wp-content/uploads/2013/02/starburst_white_300_drop_2.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.7,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 6,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 20,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 50,
+        "color": "#ffffff",
+        "opacity": 0.6,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "bottom",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 300,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode":  "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 150,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 200,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.2
+        },
+        "push": {
+          "particles_nb": 9
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
+  
+// Animation
+
+
+function animation(){
+    if (window.innerWidth > 1024) {
+    gsap.from("#title", {delay: .5, opacity: 0, y: -550, duration: 2.2})
+    gsap.from("#myButton", {delay: 2, opacity: 0, y: -550, duration: 2.2})
+    gsap.from("#pauseButton", {delay: .5, opacity: 0, x: 550, rotation: 1440, duration: 2.2})
+    gsap.from(".christmas", {delay: 4, opacity: 0, duration: 2, stagger: .6})
+    }
+    else if (window.innerWidth <= 1024 && window.innerWidth > 800){
+      gsap.from("#playButton", {delay: .5,  opacity: 0, x: -400, rotation: -1440, duration: 2.2})
+      gsap.from("#pauseButton", {delay: .5,  opacity: 0, x: 400, rotation: 1440, duration: 2.2})
+      gsap.from(".christmas", {delay: 2.8, opacity: 0, duration: 1.2, stagger: .6})  
+    }
+    else if (window.innerWidth <= 800 && window.innerWidth >500){
+      gsap.from("#playButton", {delay: .5,  opacity: 0, x: -250, rotation: -1440, duration: 2.2})
+      gsap.from("#pauseButton", {delay: .5,  opacity: 0, x: 250, rotation: 1440, duration: 2.2})
+      gsap.from(".christmas", {delay: 2.8, opacity: 0, duration: 1.2, stagger: .6})  
+    }
+    else if (window.innerWidth <= 500){
+      gsap.from("#playButton", {delay: .5,  opacity: 0, x: -90, rotation: -720, duration: 2.7})
+      gsap.from("#pauseButton", {delay: .5,  opacity: 0, x: 90, rotation: 720, duration: 2.7})
+      gsap.from(".christmas", {delay: 2.8, opacity: 0, duration: 1.2, stagger: .6})  
+    }
+  }
+  
+  animation();
